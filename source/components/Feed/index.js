f819a28736f8c72497df47a923862d58bfe1ddb1
Denys Odaruschenko <deod@ciklum.com>
2018-08-02 23:52:20 +0300

    Init

---
+++ source/components/Feed/index.js
@@ -0,0 +1,23 @@
+//Core
+import React, { Component } from 'react';
+
+//Components
+import Composer from 'components/Composer';
+import Post from 'components/Post';
+
+// Instruments
+import Styles from './styles.m.css';
+
+console.log(Styles);
+
+export default class Feed extends Component {
+    render () {
+        return (
+            <section className = { Styles.feed }>
+
+                <Composer />
+                <Post />
+            </section>
+        );
+    }
+}
